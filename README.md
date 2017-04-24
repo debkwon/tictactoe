7e319485568b7cac36f325bf679a523f236ae0ef
Deborah Kwon <deborah.m.kwon@gmail.com>
2017-04-23 20:02:21 -0400

    add game files

---
+++ README.md
@@ -0,0 +1,20 @@
+Tic Tac Toe - Play tic tac toe with a friend
+---------
+To get started:
+Fork/clone this repo
+
+Using your command line, navigate to the project folder, and open up the index file to view it in your browser. Here's an example of opening it via Chrome:
+
+open index.html -a "Google Chrome"
+
+How to Play
+---------
+
+This tic tac toe implementation is made with HTML/CSS/JavaScript. The game is for a 3x3 grid, where 3 X's or 3 O's in a row, column, or diagonal, determines a winner.
+
+You can click a box to start the game. Players take turn making their marks on the grid. Messages display the current player turn, if there's a winner, or if you need to choose a different box.
+
+A 'Restart' button allows you to start a new game and continue playing.
+
+Currently, the game is set up so that it's always X's turn first. Once you've had a winner, the winner will go first after you restart. If you wanted, you can randomize the turns during the setup.
+
---
+++ public/game.js
@@ -0,0 +1,90 @@
+//wait til the page is ready to set up the gameboard and activate the restart button
+$(document).ready(() => {
+  setupGame()
+  document.getElementById('restartButton').addEventListener('click', () => {
+    restartGame();
+  })
+});
+
+//set up the gameboard with a square of 9 boxes
+const setupGame = (firstPlayer) => {
+  let boxes = "";
+  let boxNum = 1;
+  while (boxNum < 9){
+    //add rows, with 3 td elements each
+    boxes += "<tr>";
+    for (let i = 1; i <= 3; i++){
+      boxes += `<td id='box${boxNum}' class='box' onclick='markBox(this)'></td>`;
+      boxNum++;
+    }
+    boxes += "</tr>";
+  }
+  //add all the boxes to the gameboard table
+  $('#gameboard').append(boxes)
+  document.turn = firstPlayer || "X";
+  document.winner = null;
+  setNotification(`${document.turn}'s turn`);
+}
+
+const restartGame = () => {
+  $('#gameboard').empty();
+  setupGame(document.winner);
+}
+
+const setNotification = (notice) => document.getElementById('notice').innerText = notice;
+
+const markBox = (box) => {
+  //if there's no current winner
+  if (!document.winner) {
+    //and if the box is empty, proceed with setting the box to the current player and switch player turns
+    if (!box.innerText) {
+      box.innerText = document.turn;
+      switchTurn(document.turn);
+    }
+    else setNotification("Woops, try an empty box");
+  }
+}
+
+const switchTurn = (currPlayer) => {
+  //if the checkIfWinnerIs function returns true, reassign the winner to the current player and update the notification
+  if (checkIfWinnerIs(currPlayer)) {
+    document.winner = currPlayer;
+    setNotification(`${currPlayer} won!`);
+  }
+  //if there's no winner, set the next player and notify players
+  else {
+    currPlayer === "X" ? document.turn = "O" : document.turn = "X"
+    setNotification(`${document.turn}'s turn`)
+  }
+}
+
+const checkIfWinnerIs = (player) => {
+  let result = false;
+  //check the current player with all possible winning rows
+  if (
+    checkRow(1,2,3, player) ||
+    checkRow(4,5,6, player) ||
+    checkRow(7,8,9, player) ||
+    checkRow(1,4,7, player) ||
+    checkRow(2,5,8, player) ||
+    checkRow(3,6,9, player) ||
+    checkRow(1,5,9, player) ||
+    checkRow(3,5,7, player)
+    ){
+      result = true;
+    }
+  return result;
+}
+
+const checkRow = (b1,b2,b3,player) => {
+  let hasMatchingRow = false;
+  //if the box marks for one of the winning rows matches the current player, then return true
+  if (fetchBoxMark(b1) === player && fetchBoxMark(b2) === player && fetchBoxMark(b3) === player) {
+    hasMatchingRow = true;
+  }
+  return hasMatchingRow;
+}
+
+const fetchBoxMark = (number) => document.getElementById("box" + number).innerText;
+
+
